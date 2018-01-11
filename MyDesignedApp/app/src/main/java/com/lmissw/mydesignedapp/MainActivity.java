ddc19179f4720295f4845494cfa9c40a7ff2063f
qq2216691777 <qq2216691777@qq.com>
2018-01-11 01:20:32 -0800

    there is a beautiful UI in this app

--- MyDesignedApp/app/src/main/java/com/lmissw/mydesignedapp/MainActivity.java
+++ MyDesignedApp/app/src/main/java/com/lmissw/mydesignedapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.lmissw.mydesignedapp;
 
-
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Message;
 import android.support.constraint.ConstraintLayout;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.text.format.Time;
 import android.util.Log;
 
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,7 +25,6 @@ import java.util.Date;
 import android.os.Handler;
 
 
-
 public class MainActivity extends AppCompatActivity {
     private int numTemp = 0;
 
@@ -32,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvClock = null;
     private TextView tvWeather = null;
     private ImageView ivWeather = null;
+    private ImageView ivStatus = null;
+
+    private int elcStatus = 0;
+    public final class Status {
+        /* 断电 */
+        public final static int blackOut = 1;
+        /* 应急灯 */
+        public final static int light = 2;
+        /* 无人 */
+        public final static int noPerson = 4;
+        /* 停电 */
+        public  final static int stopElc = 8;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +56,8 @@ public class MainActivity extends AppCompatActivity {
         tvClock = findViewById(R.id.clock);
         tvWeather = findViewById(R.id.weather);
         ivWeather = findViewById(R.id.imageWeather);
+        ivStatus = findViewById(R.id.eStatus);
         updateWeather();    //启动首先更新天气
-
-        Toast.makeText(MainActivity.this, "你好!", Toast.LENGTH_LONG).show();
 
         Log.i("Infor","onCreate");
 
@@ -116,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     Message msg = new Message();
                     msg.what = 1;
                     mHandler.sendMessage(msg);
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -133,12 +146,13 @@ public class MainActivity extends AppCompatActivity {
                 case 1:
                     Time t=new Time();
                     t.setToNow(); // 取得系统时间。
-                    String nowTime = t.hour+":"+t.minute;
+                    String nowTime = String.format("%02d:",t.hour)+String.format("%02d",t.minute);
                     tvClock.setText(nowTime);
                     updateDate();
 
-                    if( t.minute==0 && t.second>56 )
+                    if( t.minute%20==0 && t.second>56 )
                         updateWeather();
+
 
                     break;
                 default:
@@ -146,5 +160,124 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+
+
+    public void normalClick( View view )
+    {
+        if( (elcStatus & Status.stopElc) != 0 )
+        {
+            Log.i("Infor","已经停电了，无法进入normal");
+            return;
+        }
+        elcStatus &= ~(Status.blackOut);
+        elcStatus &= ~(Status.noPerson);
+        updateStatus();
+    }
+
+    public void noPersonClick( View view ) {
+        if(0==(elcStatus & Status.noPerson) )
+            elcStatus |= Status.noPerson;
+        else
+            elcStatus &= ~Status.noPerson;
+
+        updateStatus();
+    }
+
+    public void lightClick( View view ) {
+        if(0==(elcStatus & Status.light) )
+            elcStatus |= Status.light;
+        else
+            elcStatus &= ~Status.light;
+
+        updateStatus();
+    }
+
+    public void updateStatus()
+    {
+        if( (elcStatus & Status.noPerson) != 0 )
+        {
+         //   Log.i("Infor","无人模式");
+        }
+
+        if( (elcStatus & Status.light) != 0 ) {
+            Log.i("Infor","开灯");
+        }
+        else
+        {
+            Log.i("Infor","关灯");
+        }
+
+        if( ((elcStatus & Status.stopElc) != 0) || ((elcStatus & Status.blackOut) != 0)  )
+        {
+
+            Resources res = getResources();
+            Context ctx=getBaseContext();
+            int resId = getResources().getIdentifier("falsex", "drawable", ctx.getPackageName());
+            ivStatus.setImageDrawable( res.getDrawable(resId) );
+
+            Log.i("Infor","停电");
+
+        }
+        else if( (elcStatus & Status.noPerson) != 0 ) {
+            Resources res = getResources();
+            Context ctx=getBaseContext();
+            int resId = getResources().getIdentifier("noperson", "drawable", ctx.getPackageName());
+            ivStatus.setImageDrawable( res.getDrawable(resId) );
+            Log.i("Infor","无人模式");
+        }else{
+
+            Resources res = getResources();
+            Context ctx=getBaseContext();
+            int resId = getResources().getIdentifier("truey", "drawable", ctx.getPackageName());
+            ivStatus.setImageDrawable( res.getDrawable(resId) );
+
+            Log.i("Infor","正常模式");
+        }
+
+
+
+
+
+    }
+
+    public void blackOutClick( View view ){
+        /* @setIcon 设置对话框图标
+         * @setTitle 设置对话框标题
+         * @setMessage 设置对话框消息提示
+         * setXXX方法返回Dialog对象，因此可以链式设置属性
+         */
+
+        if((elcStatus & Status.stopElc) != 0 || ((elcStatus & Status.blackOut) != 0))
+        {
+            Toast.makeText(MainActivity.this, "电源已经被切断", Toast.LENGTH_LONG).show();
+            return;
+        }
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(MainActivity.this);
+       // normalDialog.setIcon(R.drawable.icon_dialog);
+        normalDialog.setTitle("即将切断主电源");
+        normalDialog.setMessage("切断主电源?");
+        normalDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        elcStatus |= Status.blackOut;
+                        elcStatus &= ~(Status.noPerson);
+                        Log.i("Infor","blackOutClick");
+                        updateStatus();
+                    }
+                });
+        normalDialog.setNegativeButton("取消",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        // 显示
+        normalDialog.show();
+    }
 
 }
